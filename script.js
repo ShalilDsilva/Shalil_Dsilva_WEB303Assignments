d5bd447115fa4594a880496cab23a4705c1e5553
shalil <w0810045@myscc.ca>
2023-09-18 01:33:14 -0400

    error fixes

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 /*
-	WEB 303 Assignment 1 - jQuery
-	{Shalil Dsilva}
+    WEB 303 Assignment 1 - jQuery
+    {Shalil Dsilva}
 */
 
+
 $(document).ready(function () {
-
-	$("#yearly-salary","#percent").on("change",function(){
-		var $yearlySalary = $("#yearly-salary");
-		var $percent = $("#percent");
-		var $amount = ($yearlySalary * $percent) / 100;
-		var $roundedAmount = $amount.toFixed(2);
-		$("#amount").text($roundedAmount);
-	});
-
+    $("#yearly-salary, #percent").on("change", function () {
+        var yearlySalary = $("#yearly-salary").val();
+        var percent = $("#percent").val();
+        var amount = (yearlySalary * percent) / 100;
+        var roundedAmount = amount.toFixed(2);
+        var formattedAmount = "$" + roundedAmount;
+        $("#amount").text(formattedAmount);
+    });
 });
-
