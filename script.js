cd14d2302a79677e56e3bd4326a0e37790ce5916
shalil <w0810045@myscc.ca>
2023-11-16 12:30:57 -0500

    assgn 7

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-/*
-	WEB 303 Assignment 1 - jQuery
-	{Shalil Dsilva}
-*/
-
 $(document).ready(function () {
+    // Initial data load
+    getData();
 
-	$("#yearly-salary","#percent").on("change",function(){
-		var $yearlySalary = $("#yearly-salary");
-		var $percent = $("#percent");
-		var $amount = ($yearlySalary * $percent) / 100;
-		var $roundedAmount = $amount.toFixed(2);
-		$("#amount").text($roundedAmount);
-	});
+    $('#characters-data').click(function () {
+        var field = $(this).data();
+        console.log(field)
+        // field.sort()
+    });
+
+    function getData() {
+        // $('#characters-data').text('Loading....')
+        $.ajax({
+            url: 'data.json',
+            dataType: 'json',
+            success: function (data) {
+                $('#characters-data').empty();
+
+                $.each(data, function (index, character) {
+                    var row = '<tr>';
+                    row += '<td>' + character.firstName + '</td>';
+                    row += '<td>' + character.lastName + '</td>';
+                    row += '<td>' + character.region + '</td>';
+                    row += '<td>' + character.age + '</td>';
+                    row += '<td>' + character.date + '</td>';
+                    row += '</tr>';
+                    
+                    $('#characters-data').append(row);
+                    
+
+                    
+                    
+                });
+            }
+        });
+
+
+
+        // function sortTable(field) {
+        //     var tbody = $('character-data')
+        //     tbody.sort()
+        // }
+    }
+
+
+
 
 });
 
